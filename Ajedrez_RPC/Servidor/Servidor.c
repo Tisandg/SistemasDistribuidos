464a65aee90eb82b7975c93b885cff28df46d082
reinnovik <reinnovik93@gmail.com>
2017-03-23 16:05:36 -0500

    subiendo archivos del servidor

--- Ajedrez_RPC/Servidor/Servidor.c
+++ Ajedrez_RPC/Servidor/Servidor.c
@@ -5,15 +5,103 @@
  */
 
 #include "interfaz.h"
+#include <dirent.h>
+#include <stdlib.h>
+#include <time.h>
+#define FALSO 0
+#define VERDAD 1
+
+FILE * usuarios=NULL;
+proxNodo cabeza=NULL,sigUsuario;
+fichas Todas_Fichas;
+fichas Validas_Fichas;
+fichas Servidor_fichas;
+fichas Cliente_Fichas;
+fichas Tablero;
+int vector_aux[28];
+int lados_validos[2];
+
+void Llenar_Todas_Fichas();
+void Llenar_Validas_Fichas(int);
+ficha* buscar_Ficha(int);
+void dividir(fichas, int);
+ficha* buscar_Ficha_v2(fichas, int, int, int);
+ficha* buscar_Ficha_v3(fichas , int, int);
+int Actualizar_Tablero(ficha*);
+void lados_Tablero();
+ficha* Jugada_Servidor();
 
 bool_t *
 autenticar_usuario_1_svc(datosValidar *argp, struct svc_req *rqstp)
 {
 	static bool_t  result;
+	result = FALSE;
+	int permisos = argp->permiso;
+	DIR *dir;
 
-	/*
-	 * insert server code here
-	 */
+	if(permisos==0){
+
+		printf("Buscando Administrador %d...\n",argp->login);
+	    proxNodo nuevoUsuario;
+		dir = opendir ("Administrador");
+	    /* Miramos que no haya error */
+		if (dir == NULL)
+		error("No puedo abrir el directorio");	 
+		/* Una vez nos aseguramos de que no hay error, se procede a  */
+		/* leer uno a uno todos los archivos que hay */
+
+		char path[100]="./Administrador/admin";
+		FILE * usuarioarchivo = fopen(path,"r");
+		nodo_usuario temp;
+		fread(&temp,sizeof(nodo_usuario),1,usuarioarchivo);
+		nuevoUsuario = &temp;
+		if(nuevoUsuario->login == argp->login &&  strncmp(nuevoUsuario->contrasena,argp->contrasena,10)==0){
+			printf("\n Usuario Encontrado\n");
+			result = TRUE;
+		}else{
+			result = FALSE;
+		}
+
+	}else{
+		printf("Buscando Usuario %d...\n",argp->login);
+		DIR *dir;
+		/* en *ent habrá información sobre el archivo que se está "sacando" a cada momento */
+		struct dirent *ent;
+    	proxNodo nuevoUsuario;
+
+		dir = opendir ("Usuarios");
+    	/* Miramos que no haya error */
+		if (dir == NULL)
+		error("No puedo abrir el directorio");
+		 
+		/* Una vez nos aseguramos de que no hay error, se procede a  */
+		/* leer uno a uno todos los archivos que hay */
+	
+	if (dir!=NULL){
+		while ((ent = readdir (dir)) != NULL){
+			if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+				char path[100]="./Usuarios/";
+				nodo_usuario temp;
+				strcat(path,ent->d_name);
+				FILE * usuarioarchivo = fopen(path,"r");
+				fflush(stdout);
+				fread(&temp,sizeof(nodo_usuario),1,usuarioarchivo);
+				nuevoUsuario = &temp;
+				if(nuevoUsuario->login == argp->login &&  strncmp(nuevoUsuario->contrasena,argp->contrasena,10)==0){
+					printf("\n Usuario Encontrado\n");
+					result = TRUE;
+					return &result;
+				}
+			}
+			
+		}
+	}
+	
+	printf("Usuario NO Encontrado....\n");
+	result = FALSE;
+	return &result;
+		/*------------------------------------------*/
+	}
 
 	return &result;
 }
@@ -23,9 +111,36 @@ registrarusuario_1_svc(proxNodo *argp, struct svc_req *rqstp)
 {
 	static bool_t  result;
 
-	/*
-	 * insert server code here
-	 */
+	printf("\n Creando nuevo Usuario. \n");
+	fflush(stdout);
+	nodo_usuario Nuevo_Usuario;
+	printf("%s",(*argp)->nombres);
+	fflush(stdout);
+	strcpy(Nuevo_Usuario.nombres,(*argp)->nombres);
+	strcpy(Nuevo_Usuario.apellidos,(*argp)->apellidos);
+	Nuevo_Usuario.login = (*argp)->login;
+	strcpy(Nuevo_Usuario.contrasena,(*argp)->contrasena);
+	Nuevo_Usuario.permiso = (*argp)->permiso;
+	Nuevo_Usuario.nodoSiguiente = NULL;
+
+	char archivo[1000] = "./Usuarios/usuario_";
+	/*Pasar  login a cadena*/
+	char numero[100];
+	sprintf(numero, "%d", (*argp)->login);
+	strcat(archivo,numero);
+	strcat(archivo,".txt");
+	usuarios = fopen(archivo,"a+b");
+	if(usuarios!=NULL){
+		fwrite(&Nuevo_Usuario,sizeof(nodo_usuario),1,usuarios);
+		fclose(usuarios);
+		result = TRUE;
+	}
+	else{
+		printf("\n Error al abrir el archivo");
+		result = FALSE;	
+	}
+	printf("\n Registro creado correctamente");
+	fflush(stdout);
 
 	return &result;
 }
@@ -34,10 +149,56 @@ proxNodo *
 listarusuarios_1_svc(void *argp, struct svc_req *rqstp)
 {
 	static proxNodo  result;
+	
+	free (cabeza);
+	printf("Listando usuarios...\n");
+	/* Con un puntero a DIR abriremos el directorio */
+	DIR *dir;
+	/* en *ent habrá información sobre el archivo que se está "sacando" a cada momento */
+	struct dirent *ent;
+    proxNodo nuevoUsuario;
+	char bufer [sizeof(nodo_usuario)] ;
+	dir = opendir ("Usuarios");
 
-	/*
-	 * insert server code here
-	 */
+	/* Miramos que no haya error */
+	if (dir == NULL)
+		error("No puedo abrir el directorio");
+		 
+	/* Una vez nos aseguramos de que no hay error, se procede a  */
+	/* leer uno a uno todos los archivos que hay */
+
+	if (dir!=NULL){
+			nodo_usuario temp;
+		    while ((ent = readdir (dir)) != NULL){
+				if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+					char path[1000]="./Usuarios/";
+					strcat(path,ent->d_name);
+					printf("%s \n",path);
+					FILE * usuarioarchivo = fopen(path,"r");
+					fread(&temp,sizeof(nodo_usuario),1,usuarioarchivo);
+					nuevoUsuario = (proxNodo) malloc(sizeof(nodo_usuario));
+					strcpy(nuevoUsuario->nombres,temp.nombres);
+					strcpy(nuevoUsuario->apellidos,temp.apellidos);
+					nuevoUsuario->login = temp.login;
+					strcpy(nuevoUsuario->contrasena,temp.contrasena);
+					nuevoUsuario->permiso = temp.permiso;
+					
+					if(cabeza==NULL){
+						cabeza = nuevoUsuario;
+						sigUsuario = cabeza;
+					}else{
+						sigUsuario->nodoSiguiente=nuevoUsuario;
+						sigUsuario=nuevoUsuario;
+					}
+					printf("Login: %d\n",nuevoUsuario->login);
+					sigUsuario->nodoSiguiente=NULL;
+					fclose(usuarioarchivo); 
+			    }
+			}
+	}
+	closedir (dir);
+	result = cabeza;
+	printf("Usuarios listados correctamente...\n");
 
 	return &result;
 }
@@ -46,6 +207,8 @@ bool_t *
 eliminar_usuario_1_svc(datosValidar *argp, struct svc_req *rqstp)
 {
 	static bool_t  result;
+
+	result = FALSE;
 
 	/*
 	 * insert server code here
@@ -71,10 +234,46 @@ consultarusuario_1_svc(datosValidar *argp, struct svc_req *rqstp)
 {
 	static nodo_usuario  result;
 
-	/*
-	 * insert server code here
-	 */
+	DIR *dir;
+	printf("Buscando usuario %d...\n",argp->login);
+	/* en *ent habrá información sobre el archivo que se está "sacando" a cada momento */
+	struct dirent *ent;
+    proxNodo nuevoUsuario;
 
+	dir = opendir ("Usuarios");
+    /* Miramos que no haya error */
+	if (dir == NULL)
+	error("No puedo abrir el directorio");
+		 
+	/* Una vez nos aseguramos de que no hay error, se procede a  */
+	/* leer uno a uno todos los archivos que hay */
+	
+	if (dir!=NULL){
+		while ((ent = readdir (dir)) != NULL){
+			if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+				char path[1000]="./Usuarios/";
+				nodo_usuario temp;
+				strcat(path,ent->d_name);
+				FILE * usuarioarchivo = fopen(path,"r");
+				fflush(stdout);
+				fread(&temp,sizeof(nodo_usuario),1,usuarioarchivo);
+				nuevoUsuario = &temp;
+				if(nuevoUsuario->login == argp->login &&  strncmp(nuevoUsuario->contrasena,argp->contrasena,30)==0){
+					strcpy(result.nombres,nuevoUsuario->nombres);
+					strcpy(result.apellidos,nuevoUsuario->apellidos);
+					result.login = nuevoUsuario->login;
+					strcpy(result.contrasena,nuevoUsuario->contrasena);
+					result.permiso = nuevoUsuario->permiso;
+					printf("\n Usuario Encontrado\n");
+
+					return &result;
+				}
+			}
+			
+		}
+	}
+	
+	printf("Usuario NO Encontrado....\n");
 	return &result;
 }
 
@@ -82,10 +281,18 @@ fichas *
 repartir_fichas_1_svc(int *argp, struct svc_req *rqstp)
 {
 	static fichas  result;
+	int tam = (*argp);
 
-	/*
-	 * insert server code here
-	 */
+	if(((tam % 2)==0) && (tam == 16 || tam == 18 || tam == 20 || tam == 22 || tam == 24 || tam == 26 || tam == 28)){
+		if(tam!=28){
+			Llenar_Validas_Fichas(tam);
+			dividir(Validas_Fichas, tam);
+		}else{
+			dividir(Todas_Fichas, tam);
+		}
+
+	return &Cliente_Fichas;
+	}
 
 	return &result;
 }
@@ -93,37 +300,85 @@ repartir_fichas_1_svc(int *argp, struct svc_req *rqstp)
 fichas *
 enviar_jugada_1_svc(int *argp, struct svc_req *rqstp)
 {
-	static fichas  result;
+	int id_Ficha = (*argp);
+	int repintar;
+	static ficha Nueva_Ficha;
+	static ficha Nueva_Ficha_Servidor;
 
-	/*
-	 * insert server code here
-	 */
+	if (id_Ficha == -1){
+		printf("El usuario pasa......\n");
+		printf("El servidor esta pensando su jugada....\n");
+		Nueva_Ficha_Servidor = (*Jugada_Servidor());
 
-	return &result;
+		if (Nueva_Ficha_Servidor == NULL){
+				printf("El servidor pasa .....\n");
+				return NULL;
+		}else{
+			repintar = Actualizar_Tablero(&Nueva_Ficha_Servidor);
+			if (repintar == VERDAD){
+				printf("Ficha del servidor colocada en el Tablero...\n");
+				return &Tablero;
+			}else{
+				error("Error al colocar la ficha del Servidor en el Tablero...");		
+			}
+		}
+
+		return NULL;
+
+	}else{	
+		Nueva_Ficha = (*buscar_Ficha(id_Ficha));  //Funcional
+		//Encargada de colocar la ficha en el arreglo Tablero, retorna un entero.
+		repintar = Actualizar_Tablero(&Nueva_Ficha);
+		
+		if (repintar == VERDAD){
+			printf("Ficha del usuario colocada en el Tablero...\n");
+			printf("El servidor esta pensando su jugada....\n");
+			Nueva_Ficha_Servidor = (*Jugada_Servidor());
+
+			if (Nueva_Ficha_Servidor == NULL){
+				printf("El servidor pasa .....\n");
+				return NULL;
+			}else{
+				repintar = Actualizar_Tablero(&Nueva_Ficha_Servidor);
+				if (repintar == VERDAD){
+					printf("Ficha del servidor colocada en el Tablero...\n");
+					return &Tablero;
+				}else{
+					error("Error al colocar la ficha del Servidor en el Tablero...");		
+				}
+			}
+
+		}else{
+			error("Error al colocar la ficha del usuario en el Tablero...");
+		}	
+	}	
+
+	return &Tablero;
 }
 
 fichas *
 estado_tablero_1_svc(void *argp, struct svc_req *rqstp)
 {
-	static fichas  result;
-
-	/*
-	 * insert server code here
-	 */
-
-	return &result;
+	return &Tablero;
 }
 
 fichas *
 empezar_partida_1_svc(void *argp, struct svc_req *rqstp)
 {
-	static fichas  result;
+	ficha *Nueva_Ficha;
+	int tam = tam_Vector(Servidor_fichas);
+	Nueva_Ficha = buscar_Ficha_v3(Servidor_fichas, 27, tam);
 
-	/*
-	 * insert server code here
-	 */
+	if(Nueva_Ficha == (ficha *) NULL){
+		printf("Inicia el Usuario...\n");
+	}else{
+		printf("Inicia el Servidor...\n");
+		Tablero.fichasJugadores[0].lado_A = (*Nueva_Ficha).lado_A;
+		Tablero.fichasJugadores[0].lado_B = (*Nueva_Ficha).lado_B;
+		Tablero.fichasJugadores[0].id = (*Nueva_Ficha).id;
+	}
 
-	return &result;
+	return &Tablero;
 }
 
 char **
@@ -149,3 +404,230 @@ salir_1_svc(void *argp, struct svc_req *rqstp)
 
 	return &result;
 }
+
+/*Las Funciones Locales que utilizare*/
+//Llena el vector de estructuras con todas las fichas validas del juego
+void Llenar_Todas_Fichas(){
+	int pos = 0, k = 0, p = 0;
+	for(k=0; k<7; k++){
+		for(p=k; p<7; p++){
+			Todas_Fichas.fichasJugadores[pos].lado_A=k;
+			Todas_Fichas.fichasJugadores[pos].lado_B=p;
+			Todas_Fichas.fichasJugadores[pos].id=pos;
+			pos++;
+			}
+		}
+}
+
+/*Llenamos el vector que contrendra el numero de fichas con las que se jugara*/
+void Llenar_Validas_Fichas(int tam){
+	srand(time(NULL));
+	int i=0;
+	vector_aux[i] = 27;
+	i++;
+
+	while(i<tam){
+		int randon = rand()%29;
+		if(existe(vector_aux, randon, i) == FALSO){
+			vector_aux[i] = randon;
+			i++;
+		}			 
+	}
+	i=0;
+	while(i < tam){
+		ficha *Nueva_Ficha;
+		Nueva_Ficha = buscar_Ficha(vector_aux[i]);
+		Validas_Fichas.fichasJugadores[i].lado_A = (*Nueva_Ficha).lado_A;
+		Validas_Fichas.fichasJugadores[i].lado_A = (*Nueva_Ficha).lado_B;
+		Validas_Fichas.fichasJugadores[i].id = (*Nueva_Ficha).id;
+		i++;
+	}
+}
+
+ficha* Jugada_Servidor(){
+	static ficha Nueva_Ficha;
+	lados_Tablero();
+	int tam = tam_Vector(Servidor_fichas);
+	int i = 0;
+	while(i < tam){
+		if ((Servidor_fichas.fichasJugadores[i].id != -1) && (Servidor_fichas.fichasJugadores[i].lado_A == lados_validos[0] || 
+			Servidor_fichas.fichasJugadores[i].lado_B == lados_validos[0] || Servidor_fichas.fichasJugadores[i].lado_A == lados_validos[1] || 
+			Servidor_fichas.fichasJugadores[i].lado_B == lados_validos[1])){
+			
+			Nueva_Ficha.lado_A = Servidor_fichas.fichasJugadores[i].lado_A;
+			Nueva_Ficha.lado_B = Servidor_fichas.fichasJugadores[i].lado_B;
+			Nueva_Ficha.id = Servidor_fichas.fichasJugadores[i].id;
+
+			Servidor_fichas.fichasJugadores[i].id = -1;
+
+			return &Nueva_Ficha;
+		}
+
+		i++;
+	}
+
+	return NULL;
+}
+
+ficha* buscar_Ficha(int id_Ficha){
+	static ficha Nueva_Ficha;
+
+	Nueva_Ficha.lado_A = Todas_Fichas.fichasJugadores[id_Ficha].lado_A;
+	Nueva_Ficha.lado_B = Todas_Fichas.fichasJugadores[id_Ficha].lado_B;
+	Nueva_Ficha.id = Todas_Fichas.fichasJugadores[id_Ficha].id;
+	
+	return &Nueva_Ficha;
+}
+
+/*Busca una ficha deacuerdo a los lados de la misma*/
+ficha* buscar_Ficha_v2(fichas vector_aux, int lad_A, int lad_B, int tam){
+	static ficha Nueva_Ficha;
+
+	return &Nueva_Ficha;
+}
+
+/*Busca una ficha deacuerdo al id de la misma*/
+ficha* buscar_Ficha_v3(fichas vector_aux, int id_Ficha, int tam){
+	static ficha Nueva_Ficha;
+	int i=0;
+	while(i<tam){
+		if ((vector_aux.fichasJugadores[i].id == id_Ficha)){
+			Nueva_Ficha.lado_A = vector_aux.fichasJugadores[i].lado_A;
+			Nueva_Ficha.lado_B = vector_aux.fichasJugadores[i].lado_B;
+			Nueva_Ficha.id = vector_aux.fichasJugadores[i].id;
+			return &Nueva_Ficha;
+		}
+		i++;
+	}
+	return NULL;
+}
+
+int existe(int vector[], int numero, int tam){
+	int i = 0;
+	while(i < tam){
+		if(vector[i]==numero)
+			return FALSO;
+		i++;
+	}
+	return VERDAD;
+} 
+
+void dividir(fichas obj_fichas, int tam){
+	srand(time(NULL));
+	int v_aux1[14];
+	int v_aux2[14];
+	int v_aux_usadas[28];
+	int i=0, j=0, k=0;
+
+	while(i<tam){
+		int randon = rand()%(tam+1);
+		if(existe(v_aux_usadas, randon, i) == FALSO){
+			if((i%2)==0){
+				v_aux1[j] = randon;
+				j++;
+			}else{
+				v_aux2[k] = randon;
+				k++;
+			}
+			v_aux_usadas[i] = randon;
+			i++;
+		}
+	}
+
+	i=0;
+	while(i < j){
+		Cliente_Fichas.fichasJugadores[i].lado_A = Validas_Fichas.fichasJugadores[i].lado_A;
+		Cliente_Fichas.fichasJugadores[i].lado_B = Validas_Fichas.fichasJugadores[i].lado_B;
+		Cliente_Fichas.fichasJugadores[i].id = Validas_Fichas.fichasJugadores[i].id;
+		i++;
+	}
+	i = 0;
+	while(i < k){
+		Servidor_fichas.fichasJugadores[i].lado_A = Validas_Fichas.fichasJugadores[i].lado_A;
+		Servidor_fichas.fichasJugadores[i].lado_B = Validas_Fichas.fichasJugadores[i].lado_B;
+		Servidor_fichas.fichasJugadores[i].id = Validas_Fichas.fichasJugadores[i].id;
+		i++;
+	}
+}
+
+int tam_Vector(fichas vector){
+	int tam = 0;
+	while(((vector.fichasJugadores[tam].id == '\0') && (vector.fichasJugadores[tam].lado_A == '\0') && (vector.fichasJugadores[tam].lado_B == '\0'))
+		|| ((vector.fichasJugadores[tam].id != '\0') && (vector.fichasJugadores[tam].lado_A != '\0') && (vector.fichasJugadores[tam].lado_B != '\0')))
+		tam++;
+	return tam;
+}
+
+int Actualizar_Tablero(ficha *Nueva_Ficha){
+	int tam_actual, i;
+	tam_actual = tam_Vector(Tablero);
+	lados_Tablero(); //Llamado a funcion que ctualiza los posibles valores que puede recibir el tablero
+	fichas aux_tablero;
+
+	if(lados_validos[1] == (*Nueva_Ficha).lado_A || lados_validos[1] == (*Nueva_Ficha).lado_B){
+		//Se inserta la ficha en el lado derecho
+		Tablero.fichasJugadores[tam_actual].lado_A = (*Nueva_Ficha).lado_A;
+		Tablero.fichasJugadores[tam_actual].lado_B = (*Nueva_Ficha).lado_B;
+		Tablero.fichasJugadores[tam_actual].id = (*Nueva_Ficha).id;
+		return VERDAD;
+
+	}else{
+		if (lados_validos[0] == (*Nueva_Ficha).lado_A || lados_validos[0] == (*Nueva_Ficha).lado_B){
+			//Se inserta la ficha en el lado izquierdo
+			for (i = 0; i < tam_actual; i++){
+				aux_tablero.fichasJugadores[i].lado_A = Tablero.fichasJugadores[i].lado_A;
+				aux_tablero.fichasJugadores[i].lado_B = Tablero.fichasJugadores[i].lado_B;
+				aux_tablero.fichasJugadores[i].id = Tablero.fichasJugadores[i].id;
+			}
+			Tablero.fichasJugadores[0].lado_A = (*Nueva_Ficha).lado_A;
+			Tablero.fichasJugadores[0].lado_B = (*Nueva_Ficha).lado_B;
+			Tablero.fichasJugadores[0].id = (*Nueva_Ficha).id;
+			
+			for (i = 0; i < tam_actual; i++){
+				Tablero.fichasJugadores[i+1].lado_A = aux_tablero.fichasJugadores[i].lado_A;
+				Tablero.fichasJugadores[i+1].lado_B = aux_tablero.fichasJugadores[i].lado_B;
+				Tablero.fichasJugadores[i+1].id = aux_tablero.fichasJugadores[i].id;
+			}
+			return VERDAD;
+		}else{
+			return FALSO;
+		}
+	}
+
+	return FALSO;
+}
+
+
+void lados_Tablero(){
+	int i=0;
+	int izq, der, aux_izq, aux_der, tam;
+	tam = tam_Vector(Tablero);
+
+	while(i < tam){
+		if(i=0){
+			izq = 6,
+			der = 6;
+		}else{
+			aux_izq = Tablero.fichasJugadores[i].lado_A;
+			aux_der = Tablero.fichasJugadores[i].lado_B;
+
+			if(der == aux_der)
+				der = aux_izq;
+			else
+				if(der == aux_izq)
+					der = aux_der;
+				else
+					if (izq == aux_izq)
+						izq = aux_der;
+					else
+						if(izq == aux_der)
+							izq = aux_izq;
+		}
+		i++;
+	}
+	lados_validos[0] = izq;
+	lados_validos[1] = der;
+
+}
+
+//	|4|6|--|6|6|--|6|5|--		|4|5|
